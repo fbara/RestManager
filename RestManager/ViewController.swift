ca08fe87abdac11f9c6cb683b8cadc5e078f59b4
Frank S. Bara <fbara1@yahoo.com>
2019-04-30 13:11:15 -0500

    Created a new user.

--- RestManager/ViewController.swift
+++ RestManager/ViewController.swift
@@ -15,11 +15,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getUsersList()
+        //getUsersList()
+        //getNonExistingUser()
+        createUser()
     }
     
     func getUsersList() {
         guard let url = URL(string: "https://reqres.in/api/users") else { return }
+        
+        // The following will make RestManager create the following URL:
+        // https://reqres.in/api/users?page=2
+        rest.urlQueryParameters.add(value: "2", forKey: "page")
         
         rest.makeRequest(toURL: url, withHTTPMethod: .get) { (results) in
             if let data = results.data {
@@ -27,6 +33,44 @@ class ViewController: UIViewController {
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 guard let userData = try? decoder.decode(UserData.self, from: data) else { return }
                 print(userData.description)
+            }
+            
+            print("\n\nResponse HTTP Headers:\n")
+            
+            if let response = results.response {
+                for (key, value) in response.headers.allValues() {
+                    print(key, value)
+                }
+            }
+        }
+    }
+    
+    func getNonExistingUser() {
+        guard let url = URL(string: "https://reqres.in/api/users/100") else { return }
+
+        rest.makeRequest(toURL: url, withHTTPMethod: .get) { (results) in
+            if let response = results.response {
+                if response.httpStatusCode != 200 {
+                    print("\nRequest failed with HTTP status code", response.httpStatusCode, "\n" )
+                }
+            }
+        }
+    }
+    
+    func createUser() {
+        guard let url = URL(string: "https://reqres.in/api/users") else { return }
+        
+        rest.requestHttpHeaders.add(value: "application/json", forKey: "Content-Type")
+        rest.httpBodyParameters.add(value: "Frank Bara", forKey: "name")
+        rest.httpBodyParameters.add(value: "Developer", forKey: "job")
+        
+        rest.makeRequest(toURL: url, withHTTPMethod: .post) { (results) in
+            guard let response = results.response else { return }
+            if response.httpStatusCode == 201 {
+                guard let data = results.data else { return }
+                let decoder = JSONDecoder()
+                guard let jobUser = try? decoder.decode(JobUser.self, from: data) else { return }
+                print(jobUser.description)
             }
         }
     }

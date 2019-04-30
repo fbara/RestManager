e5c7f0b6fbdd275c442f97af962046c580e6c801
Frank S. Bara <fbara1@yahoo.com>
2019-04-30 12:26:11 -0500

    Made first web request and got successful response.

--- RestManager/ViewController.swift
+++ RestManager/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let rest = RestManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getUsersList()
     }
     
+    func getUsersList() {
+        guard let url = URL(string: "https://reqres.in/api/users") else { return }
+        
+        rest.makeRequest(toURL: url, withHTTPMethod: .get) { (results) in
+            if let data = results.data {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                guard let userData = try? decoder.decode(UserData.self, from: data) else { return }
+                print(userData.description)
+            }
+        }
+    }
 }
